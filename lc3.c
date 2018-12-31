2deedb858cb3df7a71c92416d0f322af6d3241b9
mjsumpter <mjsumpter@gmail.com>
2018-12-30 23:51:47 -0500

    ADD op added

--- lc3.c
+++ lc3.c
@@ -64,7 +64,12 @@ enum
     FL_NEG = 1 << 2, /* N */
 };
 
-int main(int argc, const char* argv[])
+//Sign extend for integers less than 16 bits
+uint16_t sign_extend(uint16_t x, int bit_count);
+//update conditional flags in register
+void update_flags(uint16_t r);
+
+    int main(int argc, const char *argv[])
 {
     //Load arguments 12
     //Setup 12
@@ -85,4 +90,55 @@ int main(int argc, const char* argv[])
         {}
     }
     // shutdown 12
+}
+
+//Sign extend for integers less than 16 bits
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count - 1)) & 1)
+    {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
+//update conditional flags in register
+void update_flags(uint16_t r)
+{
+    if (reg[r] == 0)
+    {
+        reg[R_COND] = FL_ZRO;
+    }
+    else if (reg[r] >> 15) /* a 1 in the left-most bit indicates negative */
+    {
+        reg[R_COND] = FL_NEG;
+    }
+    else
+    {
+        reg[R_COND] = FL_POS;
+    }
+}
+
+
+// ADD OP
+{
+    /* destination register (DR) */
+    uint16_t r0 = (instr >> 9) & 0x7;
+    /* first operand (SR1) */
+    uint16_t r1 = (instr >> 6) & 0x7;
+    /* whether we are in immediate mode */
+    uint16_t imm_flag = (instr >> 5) & 0x1;
+
+    if (imm_flag)
+    {
+        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+        reg[r0] = reg[r1] + imm5;
+    }
+    else
+    {
+        uint16_t r2 = instr & 0x7;
+        reg[r0] = reg[r1] + reg[r2];
+    }
+
+    update_flags(r0);
 }
